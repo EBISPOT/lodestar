ea78ac92643faeba0f293a793f9964862105f8b1
Gang Fu <gangfu1982@gmail.com>
2014-11-12 15:38:45 -0500

    change namespace and sample query

--- web-ui/src/main/webapp/scripts/namespaces.js
+++ web-ui/src/main/webapp/scripts/namespaces.js
@@ -13,15 +13,8 @@
 var lodeNamespacePrefixes = {
     rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
     rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
-    owl: 'http://www.w3.org/2002/07/owl#',
     xsd: 'http://www.w3.org/2001/XMLSchema#',
-    dc: 'http://purl.org/dc/elements/1.1/',
     dcterms: 'http://purl.org/dc/terms/',
-    dbpedia2: 'http://dbpedia.org/property/',
-    dbpedia: 'http://dbpedia.org/',
-    foaf: 'http://xmlns.com/foaf/0.1/',
-    skos: 'http://www.w3.org/2004/02/skos/core#',
-
     meshv: 'http://id.nlm.nih.gov/mesh/vocab#',
     mesh: 'http://id.nlm.nih.gov/mesh/'
 };
