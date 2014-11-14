6d77230841958ef5bedc5d1df1dace69d2a26be3
Gang Fu <gangfu1982@gmail.com>
2014-11-14 09:52:28 -0500

    edited ExplorerServlet.java to enable turtle format

--- lode-core-servlet/src/main/java/uk/ac/ebi/fgpt/lode/servlet/ExplorerServlet.java
+++ lode-core-servlet/src/main/java/uk/ac/ebi/fgpt/lode/servlet/ExplorerServlet.java
@@ -99,6 +99,7 @@ public class ExplorerServlet {
                 format.equals("rdf") || format.equals("xml") ? "application/rdf+xml" :
                 format.equals("n3")                          ? "application/rdf+n3" :
                 format.equals("json")                        ? "application/rdf+json" :
+		format.equals("ttl")                         ? "application/x-turtle"
                                                                "text/plain";
             response.setContentType(out_content_type);
 
@@ -107,7 +108,8 @@ public class ExplorerServlet {
                 format.equals("rdf") || format.equals("xml") ? "RDF/XML" :
                 format.equals("n3")                          ? "N3" :
                 format.equals("json")                        ? "JSON-LD" :
-                                                               "JSON-LD" ;
+		format.equals("ttl")                         ? "TURTLE"  ;
+                                                               "N-TRIPLES" ;
             getSparqlService().query(query, format_spec, false, out);
             out.close();
         }
